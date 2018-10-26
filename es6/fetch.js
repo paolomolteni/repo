88df41ec5018f407792d04e22b6a6c311634fc35
Paolo Molteni <moltenipaolo86@gmail.com>
2018-10-26 11:09:54 +0200

    update url path

--- es6/fetch.js
+++ es6/fetch.js
@@ -2,7 +2,6 @@
 function testApi(path){
 
 	let myHeaders = new Headers();
-	myHeaders.append("Janus-Authorization", "a00c36aa-04bb-4a49-b8d5-3c91557244a5");
 	myHeaders.append("Content-Type", "application/json");
 
 	let opts = {
@@ -17,7 +16,7 @@ function testApi(path){
 	})
 	.then((data) => {
 		
-		data.page.items.forEach((item) => {
+		data.results.forEach((item) => {
 			
 			console.log(item);
 			
@@ -30,4 +29,4 @@ function testApi(path){
 //#################################################################################################
 //#################################################################################################
 
-testApi("http://localhost:8080/janus/api/customer?type=PERSON&page=1&pageSize=10");
+testApi("https://randomuser.me/api/?results=10");

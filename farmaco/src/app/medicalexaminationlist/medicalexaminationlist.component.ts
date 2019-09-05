2184bd4dd40e88bb5ac58734a60dc7dfe3618270
paolomolteni <moltenipaolo86@gmail.com>
2019-09-05 16:46:00 +0200

    updates

--- farmaco/src/app/medicalexaminationlist/medicalexaminationlist.component.ts
+++ farmaco/src/app/medicalexaminationlist/medicalexaminationlist.component.ts
@@ -4,6 +4,7 @@ import { Person } from '../model/person';
 import { MedicalExaminationService } from '../services/medicalexamination.service';
 import { PersonService } from '../services/person.service';
 import { DateUtil } from '../utils/dateutil';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-medicalexaminationlist',
@@ -13,18 +14,17 @@ import { DateUtil } from '../utils/dateutil';
 export class MedicalexaminationlistComponent implements OnInit {
 
   examinations: MedicalExamination[] = [];
-  showDetail: boolean;
   medicalExamination: MedicalExamination;
   peopleAvailable: Person[] = [];
+  popupRef: NgbModalRef;
 
   @Input()
   personId: number;
 
-  constructor(private examinationService: MedicalExaminationService, private personService: PersonService) { }
+  constructor(private examinationService: MedicalExaminationService, private personService: PersonService, private modalService: NgbModal) { }
 
   ngOnInit() {
     this.medicalExamination = new MedicalExamination();
-    this.showDetail = false;
     this.getExamination();
     this.getPeople();
   }
@@ -56,7 +56,6 @@ export class MedicalexaminationlistComponent implements OnInit {
 
   deleteExamination(examination: MedicalExamination) {
     this.examinationService.deleteExamination(examination).subscribe(res => {
-      this.closeDetail();
       this.getExamination();
       if (res.success) {
         alert('Cancellazione avvenuto con successo!');
@@ -67,38 +66,41 @@ export class MedicalexaminationlistComponent implements OnInit {
     });
   }
 
-  closeDetail(): void {
-    this.showDetail = false;
-  }
-
-  newDetail(): void {
-    this.medicalExamination = new MedicalExamination();
-    this.showDetail = true;
-  }
-
-  openDetail(examinationSelected: MedicalExamination): void {
-    this.medicalExamination = new MedicalExamination();
-    this.medicalExamination.id = examinationSelected.id;
-    this.medicalExamination.reason = examinationSelected.reason;
-    this.medicalExamination.price = examinationSelected.price;
-    this.medicalExamination.personId = examinationSelected.personId;
-    this.medicalExamination.dateCalendar = DateUtil.getData(examinationSelected.date);
-    this.showDetail = true;
-  }
-
   saveDetail(): void {
     const examinationToSave = new MedicalExamination();
     examinationToSave.id = this.medicalExamination.id;
-    examinationToSave.personId = this.personId;
+    examinationToSave.personId = this.medicalExamination.personId;
     examinationToSave.price = this.medicalExamination.price;
     examinationToSave.reason = this.medicalExamination.reason;
     examinationToSave.date = DateUtil.getDataFormatted(this.medicalExamination.dateCalendar);
 
     this.examinationService.saveExamination(examinationToSave).subscribe(res => {
-      this.closeDetail();
+      this.popupRef.close();
       this.getExamination();
     });
 
+  }
+
+  closePopup(): void {
+    this.popupRef.close();
+  }
+
+  openPopup(longContent, examination: MedicalExamination) {
+    this.medicalExamination = new MedicalExamination();
+
+    if (examination != null) {
+      this.medicalExamination.id = examination.id;
+      this.medicalExamination.reason = examination.reason;
+      this.medicalExamination.price = examination.price;
+      this.medicalExamination.personId = examination.personId;
+      this.medicalExamination.dateCalendar = DateUtil.getData(examination.date);
+    }
+
+    if(this.personId != null){
+      this.medicalExamination.personId = this.personId;
+    }
+
+    this.popupRef = this.modalService.open(longContent, { scrollable: false });
   }
 
 }

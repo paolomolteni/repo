ce8d9a5a13c87fbb90fab681e5d359aa85c9d4f8
paolomolteni <moltenipaolo86@gmail.com>
2019-11-28 15:04:47 +0100

    changed packaging property(jar to war)

--- food/src/main/java/it/paolomolteni/food/FoodApplication.java
+++ food/src/main/java/it/paolomolteni/food/FoodApplication.java
@@ -2,9 +2,10 @@ package it.paolomolteni.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FoodApplication {
+public class FoodApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodApplication.class, args);
